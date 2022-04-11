62af7b12ec819a77bace2bb2a7171c0ccb2a20ac
cindywangg <wangcindy71@gmail.com>
2022-04-11 17:06:43 +0000

    rrt

--- src/rrt.py
+++ src/rrt.py
@@ -31,6 +31,7 @@ class RRT:
                  goal,
                  obstacle_list,
                  rand_area,
+                 occupancy_grid,
                  expand_dis=3.0,
                  path_resolution=0.5,
                  goal_sample_rate=5,
@@ -52,14 +53,13 @@ class RRT:
         self.max_rand = rand_area[0]
         self.node_list = [] 
         self.max_iter = 500
+        self.occupancy_grid = occupancy_grid
 
     def get_random_node(self):
-        
         if random.randint(0,100) > self.get_random_node:
-            #Used to be random.uniform!
             rnd = self.Node(
-                random.uniform(self.min_rand, self.max_rand),
-                random.uniform(self.min_rand, self.max_rand))
+                math.floor(random.uniform(self.min_rand, self.max_rand)),
+                math.floor(random.uniform(self.min_rand, self.max_rand)))
         else:
             rnd = self.Node(self.end.x, self.end.y)
 
@@ -69,13 +69,44 @@ class RRT:
         dlist = [(node.x - rnd_node.x)**2 + (node.y - rnd_node.y)**2
                  for node in node_list]
         minind = dlist.index(min(dlist))
-
-
         return minind
 
-    #TODO
-    def detect_collisions (node,obstacleList):
-        pass
+    def sign(n):
+        return (n > 0) - (n < 0)
+
+    def check_collision(self, start, end ):
+    #ray tracing taken from here 
+    #https://stackoverflow.com/questions/35807686/find-cells-in-array-that-are-crossed-by-a-given-line-segment
+        (xA, yA) = start.x, start.y
+        (xB, yB) = end.x, end.y 
+        (dx, dy) = (xB - xA, yB - yA)
+        (sx, sy) = (self.sign(dx), self.sign(dy))
+
+        grid_A = (math.floor(xA), math.floor(yA))
+        grid_B = (math.floor(xB), math.floor(yB))
+        (x, y) = grid_A
+        traversed=[grid_A]
+
+        tIx = dy * (x + sx - xA) if dx != 0 else float("+inf")
+        tIy = dx * (y + sy - yA) if dy != 0 else float("+inf")
+
+        while (x,y) != grid_B:
+            # NB if tIx == tIy we increment both x and y
+            (movx, movy) = (tIx <= tIy, tIy <= tIx)
+
+            if movx:
+                # intersection is at (x + sx, yA + tIx / dx^2)
+                x += sx
+                tIx = dy * (x + sx - xA)
+
+            if movy:
+                # intersection is at (xA + tIy / dy^2, y + sy)
+                y += sy
+                tIy = dx * (y + sy - yA)
+
+            traversed.append( (x,y) )
+
+        return traversed
 
     def calc_distance_and_angle(from_node, to_node):
         dx = to_node.x - from_node.x
@@ -89,10 +120,6 @@ class RRT:
         dy = y - self.end.y
         return math.hypot(dx, dy)
 
-
-
-
-
     def generate_final_course(self, goal_ind):
         path = [[self.end.x, self.end.y]]
         node = self.node_list[goal_ind]
@@ -103,7 +130,6 @@ class RRT:
 
         return path
 
-#TODO    
     def planning(self):
         self.node_list = [self.start]
         for i in range(self.max_iter):
@@ -111,50 +137,31 @@ class RRT:
             nearest_ind = self.get_nearest_node_index(self.node_list, rnd_node)
             nearest_node = self.node_list[nearest_ind]
 
-            new_node = self.steer(nearest_node, rnd_node, self.expand_dis)
+            new_node = self.steer(nearest_node, rnd_node)
 
             if self.check_if_outside_play_area(new_node, self.play_area) and \
-                self.check_collision(new_node, self.obstacle_list):
+                self.check_collision(nearest_node, new_node):
                 self.node_list.append(new_node)
 
-            # check if the node we are at is the goal node
-            # if self.calc_dist_to_goal(self.node_list[-1].x,
-            #                             self.node_list[-1].y) <= self.expand_dis:
-            if self.node_list[-1].x == self.end.x and self.node_list[-1].y == self.end.y:
-                return self.generate_final_course(len(self.node_list) - 1)
+                if new_node.x == self.end.x and new_node.y == self.end.y:
+                    return self.generate_final_course(len(self.node_list) - 1)
 
-            # if animation and i % 5:
-            #     self.draw_graph(rnd_node)
         return None  # cannot find path
 
-    def steer(self, from_node, to_node, extend_length=float("inf")):
+    def steer(self, from_node, to_node):
         new_node = self.Node(from_node.x, from_node.y)
         d, theta = self.calc_distance_and_angle(new_node, to_node)
 
-        new_node.path_x = [new_node.x]
-        new_node.path_y = [new_node.y]
-
-        if extend_length > d:
-            extend_length = d 
-
         if self.path_resolution < d:
             ratio = self.path_resolution/d
+            new_node.x += math.floor(d * ratio * math.cos(theta))
+            new_node.y += math.floor(d * ratio * math.sin(theta))
         else:
-            ratio = d/self.path_resolution
-
-        new_node.x += d * ratio * math.cos(theta)
-        new_node.y += d * ratio * math.sin(theta)
-
-        # for _ in range(n_expand):
-        #     new_node.x += self.path_resolution * math.cos(theta)
-        #     new_node.y += self.path_resolution * math.sin(theta)
-        #     new_node.path_x.append(new_node.x)
-        #     new_node.path_y.append(new_node.y)
+            new_node.x = to_node.x
+            new_node.y = to_node.y
 
         new_node.parent = from_node
-
         return new_node
-
 
 occupancy_grid_ex = np.array([
     [0,0,0,0,0,1,0,0,0,0],
