bb82b9e1cf10abf78271b537bb73bcd66c695049
reyfp <rey40fp@gmail.com>
2022-04-12 20:54:17 +0000

    RRT basic example working

--- src/rrt.py
+++ src/rrt.py
@@ -35,7 +35,7 @@ class RRT:
                  expand_dis=3.0,
                  path_resolution=0.5,
                  goal_sample_rate=5,
-                 max_iter=500,
+                 max_iter=5000000,
                  play_area = None
                  ):
         """
@@ -46,7 +46,7 @@ class RRT:
         randArea could be the whole map!
         play_area:stay inside this area [xmin,xmax,ymin,ymax]
         """
-        self.directions = [[-1,-1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1,1]] ##????
+        
         self.start = self.Node(start[0],start[1])
         self.end = self.Node(goal[0],goal[1])
         self.min_rand = rand_area[0]
@@ -73,42 +73,36 @@ class RRT:
         minind = dlist.index(min(dlist))
         return minind
 
-    def sign(self,n):
-        return (n > 0) - (n < 0)
 
-    def check_collision(self, start, end ):
-    #ray tracing taken from here 
-    #https://stackoverflow.com/questions/35807686/find-cells-in-array-that-are-crossed-by-a-given-line-segment
-        (xA, yA) = start.x, start.y
-        (xB, yB) = end.x, end.y 
-        (dx, dy) = (xB - xA, yB - yA)
-        (sx, sy) = (self.sign(dx), self.sign(dy))
+    def check_collisions_improved(self,start,end):
+        if (start.x,start.y) == (end.x,end.y):
+            return True
+        step_size_movement = 1  #This is how far we move in x and y on every step
+        path_list = []
+        d,theta = self.calc_distance_and_angle(start, end)
+        print("Theta between",(start.x,start.y),"and ",(end.x,end.y),"is",theta)
+        conti = True
+        x_start = start.x
+        y_start = start.y
+        # print(x_start,y_start)
+        while conti:
+            step_x = step_size_movement*math.cos(theta)
+            step_y = step_size_movement*math.sin(theta)
+            print("Steps in x and y", step_x,step_y)
+            print("Theta",theta)
+            # print([int(x_start+step_x),int(y_start+step_y)])
+            if self.occupancy_grid[int(x_start+step_x),int(y_start+step_y)] == 1:  ## CHANGE 1 TO OCCUPANCY GRID PERCENT!
+                return False
+            else:
+                print("Current pos",[int(x_start+step_x),int(y_start+step_y)] ,"End: ",[end.x,end.y])
+                og_shape = self.occupancy_grid.shape
+                if int(x_start+step_x)==int(end.x) and int(y_start+step_y) == int(end.y):
+                    conti = False
+                x_start += step_x
+                y_start += step_y
+                
+        return True
 
-        grid_A = (math.floor(xA), math.floor(yA))
-        grid_B = (math.floor(xB), math.floor(yB))
-        (x, y) = grid_A
-        traversed=[grid_A]
-
-        tIx = dy * (x + sx - xA) if dx != 0 else float("+inf")
-        tIy = dx * (y + sy - yA) if dy != 0 else float("+inf")
-
-        while (x,y) != grid_B:
-            # NB if tIx == tIy we increment both x and y
-            (movx, movy) = (tIx <= tIy, tIy <= tIx)
-
-            if movx:
-                # intersection is at (x + sx, yA + tIx / dx^2)
-                x += sx
-                tIx = dy * (x + sx - xA)
-
-            if movy:
-                # intersection is at (xA + tIy / dy^2, y + sy)
-                y += sy
-                tIy = dx * (y + sy - yA)
-
-            traversed.append( (x,y) )
-
-        return traversed
 
     def calc_distance_and_angle(self,from_node, to_node):
         dx = to_node.x - from_node.x
@@ -136,12 +130,15 @@ class RRT:
         self.node_list = [self.start]
         for i in range(self.max_iter):
             rnd_node = self.get_random_node()
+            print("Random Generated Node",rnd_node.x,rnd_node.y)
             nearest_ind = self.get_nearest_node(self.node_list, rnd_node) # Changed get_nearest_node_index for get_nearest_node
+            
             nearest_node = self.node_list[nearest_ind]
+            print('Nearest Node',nearest_node.x,nearest_node.y)
 
             new_node = self.steer(nearest_node, rnd_node)
-
-            if self.check_collision(nearest_node, new_node): # Deleted self.check_if_outside_play_area(new_node, self.play_area)
+            
+            if self.check_collisions_improved(nearest_node, new_node): # Deleted self.check_if_outside_play_area(new_node, self.play_area)
                 self.node_list.append(new_node)
 
                 if new_node.x == self.end.x and new_node.y == self.end.y:
@@ -152,11 +149,12 @@ class RRT:
     def steer(self, from_node, to_node):
         new_node = self.Node(from_node.x, from_node.y)
         d, theta = self.calc_distance_and_angle(new_node, to_node)
-
+        print("Distance d:",d)
         if self.path_resolution < d:
-            ratio = self.path_resolution/d
-            new_node.x += d * ratio * math.cos(theta) ## DELETED MATH FLOOR. SIZES <1 GET CONVERTED INTO 0
-            new_node.y += d * ratio * math.sin(theta)
+            # ratio = self.path_resolution/d
+            new_node.x += math.floor(self.path_resolution * math.cos(theta)) ## DELETED MATH FLOOR. SIZES <1 GET CONVERTED INTO 0
+            new_node.y += math.floor(self.path_resolution * math.sin(theta))
+            print("X and Y:",new_node.x,new_node.y)
         else:
             new_node.x = to_node.x
             new_node.y = to_node.y
@@ -176,7 +174,33 @@ occupancy_grid_ex = np.array([
     [0,0,1,0,0,0,0,1,0,0],
     [0,0,1,0,0,0,0,1,0,0]
     ])
-
+occupancy_grid_ex_2 = np.array([
+    [0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
+    [0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
+    [0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
+    [0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
+    [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
+    [0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
+    [0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
+    [0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
+    [0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
+    ])
 obstacle_l = [(5, 0), (5, 1), (2, 2), (5, 2), (2, 3), (5, 3), (2, 4), (5, 4), (2, 5), 
     (2, 6), (2, 7), (2, 8), (7, 8), (2, 9), (7, 9)]
 start = [0,0]
@@ -184,6 +208,8 @@ start = [0,0]
 goal =  [8,8]
 iter_counter = 0
 
-rrt = RRT(start=[0, 0],  goal=[8, 8 ], rand_area = [0,10],occupancy_grid = occupancy_grid_ex, obstacle_list = obstacle_l)
+rrt = RRT(start=[0, 0],  goal=[8, 8 ], rand_area = [0,9],occupancy_grid = occupancy_grid_ex, obstacle_list = obstacle_l,path_resolution = 1)
+# rrt = RRT(start=[0, 0],  goal=[7, 22 ], rand_area = [0,24],occupancy_grid = occupancy_grid_ex_2, obstacle_list = obstacle_l,path_resolution = 1)
+
 path = rrt.planning()
 print(path)
